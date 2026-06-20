7c5aba8daba5dc76655e913c047d1cdcde867e07
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-06-19 22:08:42 -0700

    Enhance logging & performance

--- Gd/Globals/Constants.cs
+++ Gd/Globals/Constants.cs
@@ -1,9 +1,11 @@
+// ReSharper disable MemberCanBePrivate.Global
+
 namespace Root.Gd.Globals;
 
 public static class Constants
 {
-	private const string UserScheme = "user://";
-	private const string ResourceScheme = "res://";
+	public const string UserScheme = "user://";
+	public const string ResourceScheme = "res://";
 
 	public const string UserDataCfgPath = UserScheme + "user_data.cfg";
 	public const string AssetsDir = ResourceScheme + "Assets";

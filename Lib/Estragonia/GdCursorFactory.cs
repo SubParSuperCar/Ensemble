09953606f18b0a847d0ec05ffb9eb472a1ce6796
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-06-21 14:54:23 -0700

    Add Estragonia.csproj

---
+++ Lib/Estragonia/GdCursorFactory.cs
@@ -0,0 +1,15 @@
+using System;
+using Avalonia;
+using Avalonia.Input;
+using Avalonia.Platform;
+
+namespace Estragonia;
+
+internal sealed class GodotCursorFactory : ICursorFactory
+{
+	public ICursorImpl GetCursor(StandardCursorType cursorType) =>
+		new GodotStandardCursorImpl(cursorType.ToGodotCursorShape());
+
+	public ICursorImpl CreateCursor(IBitmapImpl cursor, PixelPoint hotSpot) =>
+		throw new NotSupportedException("Custom cursors aren't supported");
+}

1bafb91d13d346de000ee612a20e856f5f9544eb
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-07-24 04:38:03 -0700

    Add TODO comments for note-taking purposes

--- ui/impl/Designer.cs
+++ ui/impl/Designer.cs
@@ -10,7 +10,7 @@ public static class Designer
 		throw new NotSupportedException("This project isn't meant to be run; it's only for Avalonia designer support.");
 
 	// ReSharper disable once UnusedMember.Global
-	// TODO
+	// TODO: Fix SkiaSharp .UseSkia()
 	public static AppBuilder BuildAvaloniaApp() =>
 		AppBuilder
 			.Configure<App>()

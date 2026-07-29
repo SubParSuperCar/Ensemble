97044ccd046f6e39b6085fea3f03e033baa4c850
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-07-29 01:29:28 -0700

    Manually update UI color theme to match OS at runtime

--- ui/impl/App.axaml.cs
+++ ui/impl/App.axaml.cs
@@ -1,9 +1,30 @@
 using Avalonia;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using Avalonia.Styling;
 
 namespace Root.Ui.Impl;
 
 public class App : Application
 {
 	public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
+	// TODO
+	public override void OnFrameworkInitializationCompleted()
+	{
+		if (RequestedThemeVariant == ThemeVariant.Default && PlatformSettings is not null)
+		{
+			UpdateTheme(PlatformSettings.GetColorValues());
+			PlatformSettings.ColorValuesChanged += (_, colorValues) => UpdateTheme(colorValues);
+		}
+
+		base.OnFrameworkInitializationCompleted();
+	}
+
+	private void UpdateTheme(PlatformColorValues colorValues) =>
+		RequestedThemeVariant = colorValues.ThemeVariant switch
+		{
+			PlatformThemeVariant.Dark => ThemeVariant.Dark,
+			_ => ThemeVariant.Light
+		};
 }

02dd5d6ff5fde99aa198b040b98a133c4cbce968
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-07-31 17:47:38 -0700

    Adjust OS exclusivity in WebViewSetup.cs

--- ui/impl/WebViewSetup.cs
+++ ui/impl/WebViewSetup.cs
@@ -7,9 +7,6 @@ public static class WebViewSetup
 	[ModuleInitializer]
 	public static void PrepareWebView()
 	{
-		if (!OperatingSystem.IsLinux())
-			return;
-
 		Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS",
 			"--no-sandbox --disable-gpu-sandbox --disable-features=WebAssemblyTrapHandler");
 		Environment.SetEnvironmentVariable("WPE_BCM_DECLARE_BUFFERS", "0");

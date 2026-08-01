e69b57a2a61f790bab30deae2e3852368b19dbaf
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-08-01 01:56:21 -0700

    Remove custom Environment Variables

--- ui/impl/WebViewPreparer.cs
+++ ui/impl/WebViewPreparer.cs
@@ -7,9 +7,6 @@ public static class WebViewPreparer
 	[ModuleInitializer]
 	public static void PrepareWebView()
 	{
-		Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS",
-			"--no-sandbox --disable-gpu-sandbox --disable-features=WebAssemblyTrapHandler");
-		Environment.SetEnvironmentVariable("WPE_BCM_DECLARE_BUFFERS", "0");
-		Environment.SetEnvironmentVariable("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+		// Ignore
 	}
 }

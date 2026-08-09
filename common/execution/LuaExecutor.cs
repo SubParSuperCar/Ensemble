24cdd84080b15fc6bfb1643c12ddc043606df87e
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-08-08 21:30:04 -0700

    Give LuaExecutor.cs 'Execute' an optional CancellationToken parameter

--- common/execution/LuaExecutor.cs
+++ common/execution/LuaExecutor.cs
@@ -6,7 +6,9 @@ namespace Root.Common.Execution;
 
 public static partial class LuaExecutor
 {
-	public static async Task<LuaValue[]> Execute(string source)
+	public static async Task<LuaValue[]> Execute(
+		string source,
+		CancellationToken cancellationToken = default)
 	{
 		source = source.Trim();
 		Log.Information(">\n{Source}", source);
@@ -16,7 +18,7 @@ public static partial class LuaExecutor
 
 		AddFunctions(state.Environment);
 
-		var results = await state.DoStringAsync(source).ConfigureAwait(false);
+		var results = await state.DoStringAsync(source, cancellationToken: cancellationToken).ConfigureAwait(false);
 		Log.Information("< [{Results}]", string.Join(", ", results.Select(v => v.ToString())));
 
 		return results;

cd7cee7f931ea328138ef75574b20b3ff0609ac8
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-08-09 17:45:15 -0700

    Add new meshes & build assets, courtesy of "shrimp fried koishi"

--- scripts/game/Game.cs
+++ scripts/game/Game.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Serilog;
 
 namespace Root.Scripts.Game;
 
@@ -9,17 +10,31 @@ public partial class Game : Node3D
 	public override void _Ready()
 	{
 #if DEBUG
-		const int plotId = 2;
-		var plot = GPlots.Get(plotId);
-		var instances = plot!.Instances;
+		{
+			const int plotId = 2;
+			var plot = GPlots.Get(plotId);
+			var instances = plot!.Instances;
 
-		const int assetId = 0;
-		var position = Vector3.Down * 15;
-		var rotation = Quaternion.Identity;
-		instances.Add(assetId, position, rotation);
+			const int assetId = 0;
+			var position = Vector3.Down * 15;
+			var rotation = Quaternion.Identity;
+			instances.Add(assetId, position, rotation);
 
-		GPlayers.Add(string.Empty, "Larpje139 (Test)");
-		GPlayers.Add(string.Empty, "Pepsi bottle guzzler (Test)");
+			GPlayers.Add(string.Empty, "Larpje139 (Test)");
+			GPlayers.Add(string.Empty, "Pepsi bottle guzzler (Test)");
+		}
 #endif
+
+		Log.Debug("{Players}:", nameof(GPlayers));
+		foreach (var player in GPlayers.GetAll())
+			Log.Debug("{$Player}", player.ToDict());
+
+		Log.Debug("{Assets}:", nameof(GAssets));
+		foreach (var asset in GAssets.GetAll())
+			Log.Debug("{$Asset}", asset.ToDict());
+
+		Log.Debug("{Plots}:", nameof(GPlots));
+		foreach (var plot in GPlots.GetAll())
+			Log.Debug("{$Plot}", plot.ToDict());
 	}
 }

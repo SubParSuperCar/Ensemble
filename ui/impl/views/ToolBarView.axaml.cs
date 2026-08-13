aa63049526a64c969d9412a03152e7741904147c
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-08-13 04:11:21 -0700

    Add tiny optimizations and refinements

--- ui/impl/views/ToolBarView.axaml.cs
+++ ui/impl/views/ToolBarView.axaml.cs
@@ -4,7 +4,6 @@ using Root.Ui.Impl.ViewModels;
 
 namespace Root.Ui.Impl.Views;
 
-// ReSharper disable once UnusedType.Global
 public partial class ToolBarView : UserControl, IViewFor<ToolBarViewModel>
 {
 	public ToolBarView()

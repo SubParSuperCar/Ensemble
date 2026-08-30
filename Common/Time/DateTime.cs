bb8c28c6df777936508e4dfc50c0d69b5d546b24
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2026-08-29 17:25:39 -0700

    Use a readonly for DateTime.cs TimeProvider instead of a getter

--- Common/Time/DateTime.cs
+++ Common/Time/DateTime.cs
@@ -2,5 +2,5 @@ namespace Root.Common.Time;
 
 public static class DateTime
 {
-	public static WrappedTimeProvider TimeProvider { get; } = new();
+	public static readonly WrappedTimeProvider TimeProvider = new();
 }

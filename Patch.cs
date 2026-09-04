945667f1924e0b41740bcd1b60dfb41b2f6568e6
elio <elliot_grimm_2008@yahoo.com>
2026-09-04 01:30:56 -0700

    Add Patch.cs to fix export layout on initial export-time run

---
+++ Patch.cs
@@ -0,0 +1,47 @@
+#if EXPORT
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Root;
+
+public static class Patch
+{
+	[ModuleInitializer]
+	public static void Initialize()
+	{
+		try
+		{
+			Console.WriteLine("Patching export layout...");
+			var stopwatch = Stopwatch.StartNew();
+
+			var exeDir = Path.GetDirectoryName(Environment.ProcessPath);
+			if (exeDir is null)
+				return;
+
+			foreach (var dataDir in Directory.EnumerateDirectories(exeDir, "data*", SearchOption.TopDirectoryOnly))
+				MoveContents(dataDir, exeDir);
+
+			stopwatch.Stop();
+			Console.WriteLine(string.Create(CultureInfo.InvariantCulture,
+				$"Patched export layout in {stopwatch.Elapsed.TotalMilliseconds:F3} ms."));
+		}
+		catch (Exception exception)
+		{
+			Console.Error.WriteLine($"Failed to patch export layout:\n{exception}");
+		}
+	}
+
+	private static void MoveContents(string source, string destination)
+	{
+		foreach (var file in Directory.EnumerateFiles(source, "*", SearchOption.AllDirectories))
+		{
+			var target = Path.Combine(destination, Path.GetRelativePath(source, file));
+			Directory.CreateDirectory(Path.GetDirectoryName(target)!);
+
+			File.Move(file, target, true);
+		}
+	}
+}
+
+#endif

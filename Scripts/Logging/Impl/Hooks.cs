6a7d26459a2ca0ba23c15e788a471107103485d2
elio <elliot_grimm_2008@yahoo.com>
2026-09-18 19:35:12 -0700

    Improve SeriLog logging by writing headers & filling in args
    Signed-off-by: elio <elliot_grimm_2008@yahoo.com>

---
+++ Scripts/Logging/Impl/Hooks.cs
@@ -0,0 +1,11 @@
+using Serilog.Sinks.File.Header;
+
+namespace Root.Scripts.Logging.Impl;
+
+// ReSharper disable once UnusedType.Global
+public static class Hooks
+{
+	// ReSharper disable once UnusedMember.Global
+	public static HeaderWriter Header =>
+		new("{\"@header\":\"This is an Ensemble SeriLog file: https://github.com/SubParSuperCar/Ensemble\"}\n");
+}
